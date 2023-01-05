f6fe9ab38ea158608558653b9b02c5943ed6ea63
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2023-01-05 22:36:18 +0300

    Authentication islemleri yapildi

--- Presentation/ECommerceAPI/Controllers/UsersController.cs
+++ Presentation/ECommerceAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Features.Commands.AppUser.CreateUser;
+using ECommerceAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,13 @@ namespace ECommerceAPI.Controllers
         public async Task<IActionResult> CreateUser(CreateUserCommandRequest createUserCommandRequest)
         {
             CreateUserCommandResponse response = await _mediator.Send(createUserCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
             return Ok(response);
         }
     }
