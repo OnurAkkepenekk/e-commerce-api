ad0c125cbdc11906fc0217babbd12d84d11abacd
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2023-01-07 16:48:41 +0300

    Google login islemleri tamamlandi

---
+++ Core/ECommerceAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using ECommerceAPI.Application.Abstractions.Token;
+using ECommerceAPI.Application.DTOs;
+using Google.Apis.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Commands.AppUser.GoogleLogin
+{
+    public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+
+        public GoogleLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+        }
+
+        public async Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { "654971576237-0vidj7eh31q2fm5rgnkbge3sr730puor.apps.googleusercontent.com" }
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+
+            var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
+            Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+            bool result = user != null;
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(payload.Email);
+                if (user == null)
+                {
+                    user = new()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = payload.Email,
+                        UserName = payload.Email,
+                        NameSurname = payload.Name
+                    };
+                    var identityResult = await _userManager.CreateAsync(user);
+                    result = identityResult.Succeeded;
+                }
+            }
+
+            if (result)
+                await _userManager.AddLoginAsync(user, info); //AspNetUserLogins
+            else
+                throw new Exception("Invalid external authentication.");
+
+            Token token = _tokenHandler.CreateAccessToken(5);
+
+            return new()
+            {
+                Token = token
+            };
+        }
+    }
+}
--- Presentation/ECommerceAPI/Program.cs
+++ Presentation/ECommerceAPI/Program.cs
@@ -63,7 +63,7 @@ app.UseCors();
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();    
+app.UseAuthentication();     
 app.UseAuthorization();
 
 app.MapControllers();
